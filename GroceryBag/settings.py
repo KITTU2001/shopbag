00d5aa35157ede1706493777e7414d214e882a7b
KITTU2001 <60604872+KITTU2001@users.noreply.github.com>
2021-12-24 15:52:04 +0530

    Update settings.py

--- GroceryBag/settings.py
+++ GroceryBag/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','shopbaggrocery.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','shopbag123.herokuapp.com']
 
 
 # Application definition
